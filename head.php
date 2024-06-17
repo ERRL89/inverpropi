1d5cfee1372bb9cef70f155b019b2070df2b1537
Edson Rubio <ing.edson.rubio@outlook.com>
2024-06-16 20:48:30 -0600

    UPDATE: Version Final de pagina

--- head.php
+++ head.php
@@ -1,22 +1,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="copyright" content="Vidrio, Aluminio, Fabricación, Instalacion, Puertas, Ventanas, Domos, Fachadas, Pergolas, Barandales, Herreria, Cristal Templado, Vidrio Templado Chihuahua">
+    <meta name="copyright" content="Inversiones inmobiliarias, bienes raíces, compra colectiva, proyectos inmobiliarios, remates bancarios, propiedades residenciales, locales comerciales, terrenos, ingresos pasivos, apreciación del valor, Mexico">
     <meta name="audience" content="all">
     <meta name="robots" content="index, all, follow">
     <meta name="category"
-        content="Vidrio, Aluminio, Fabricación, Instalacion, Puertas, Ventanas, Domos, Fachadas, Pergolas, Barandales, Herreria, Cristal Templado, Vidrio Templado Chihuahua">
-    <meta itemprop="telephone" content="6145294754">
+        content="Inversiones, Bienes Raíces, Finanzas, Propiedades, Inmuebles, Compra Colectiva, Proyectos Inmobiliarios, Remates Bancarios, Propiedades Residenciales, Locales Comerciales, Terrenos, Ingresos Pasivos, Apreciación del Valor, Crecimiento Patrimonial, Estabilidad Financiera, Rentabilidad, Oportunidades de Inversión, Mercado Inmobiliario, Capital Inicial, Estrategias de Inversión Mexico">
+    <meta itemprop="telephone" content="5533951859">
     <meta name="descripcion"
-        content="Soluciones en Vidrio y Aluminio, Fabricación e Instalacion de Puertas Ventanas Domos Pergolas Barandales Herreria Cristal Templado Vidrio Templado Fachadas  Chihuahua">
+        content="Inver Propi ofrece oportunidades de inversión en bienes raíces, incluyendo compra colectiva, remates bancarios y proyectos inmobiliarios accesibles. Invierte con poco dinero y maximiza tus ganancias con nuestras estrategias de inversión inteligente y crecimiento patrimonial">
 
     <meta name="keywords"
-        content="Vidrio, Aluminio, Fabricación, Instalacion, Puertas, Ventanas, Domos, Fachadas, Pergolas, Barandales, Herreria, Cristal Templado, Vidrio Templado Chihuahua">
+        content="Inversiones inmobiliarias, bienes raíces, compra colectiva, proyectos inmobiliarios, remates bancarios, propiedades residenciales, locales comerciales, terrenos, ingresos pasivos, apreciación del valor, crecimiento patrimonial, estabilidad financiera, rentabilidad, oportunidades de inversión, mercado inmobiliario, capital inicial, estrategias de inversión, Inver Propi, inversión accesible, inversión rentable">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <link rel="stylesheet" href="./css/style.css" />
-    <link rel="shortcut icon" href="./images/logo.jpg" type="image/x-icon" />
+    <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
